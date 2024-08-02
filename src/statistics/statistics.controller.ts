71a39908573620fc2b4dc96e0298650d11e9671a
Samandar Sunnatov <sunnatovsamandar77@gmail.com>
2024-08-02 19:36:01 +0500

    added swagger for stats

--- src/statistics/statistics.controller.ts
+++ src/statistics/statistics.controller.ts
@@ -1,25 +1,48 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { StatisticsService } from './statistics.service';
 
+
+@ApiTags('Statistics')
 @Controller('stats')
 export class StatisticsController {
   constructor(private readonly statisticsService: StatisticsService) {}
 
+  @ApiOperation({ summary: 'Get user statistics by month' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful response',
+  })
   @Get('users-by-month')
   async getUserStatsByMonth() {
     return this.statisticsService.getUserStatsByMonth();
   }
 
+  @ApiOperation({ summary: 'Get driver statistics by month' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful response',
+  })
   @Get('drivers-by-month')
   async getDriverStatsByMonth() {
     return this.statisticsService.getDriverStatsByMonth();
   }
 
+  @ApiOperation({ summary: 'Get taxi order statistics by month' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful response',
+  })
   @Get('order-taxi-by-month')
   async getOrderTaxiStatsByMonth() {
     return this.statisticsService.getOrderTaxiStatsByMonth();
   }
 
+  @ApiOperation({ summary: 'Get truck order statistics by month' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful response',
+  })
   @Get('order-truck-by-month')
   async getOrderTruckStatsByMonth() {
     return this.statisticsService.getOrderTruckStatsByMonth();
